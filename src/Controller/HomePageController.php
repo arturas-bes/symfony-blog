36ac88005f21ce6ea4f3ebc45467482e532cd2ca
Arturas Bespalovas <artazzs1@gmail.com>
2018-12-10 19:18:46 +0200

    contact form added, translations updated

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
   
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-  
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;  
 class HomePageController extends Controller
 {
     /**
@@ -22,12 +26,41 @@ class HomePageController extends Controller
         return $this->render('about/about.html.twig');
     }
   
-    /**
-     * @Route("/contact", name="contact")
-     */
-    public function contact() {
-        return $this->render('contact/contact.html.twig');
+   /**
+ * @Route("/contact", name="contact")
+ * @Method("GET|POST")
+ */
+    public function contact(Request $request)
+    {
+    $enquiry = new Contact();
+    $form = $this->createForm(ContactType::class, $enquiry);
+ 
+     $this->request = $request;
+        if ($request->getMethod() == 'POST') {
+        $form->bind($request);
+ 
+ 
+        if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from Example')
+                    ->setFrom('contact@example.com')
+                    ->setTo($this->container->getParameter('app.emails.contact_email'))
+                    ->setBody($this->renderView('contact/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+             
+            $this->get('session')->getFlashbag('blog-notice', 'Your contact enquiry was successfully sent. Thank you!');
+  
+  
+        // Redirect - This is important to prevent users re-posting
+        // the form if they refresh the page
+        return $this->redirect($this->generateUrl('contact'));
     }
+    }
+ 
+    return $this->render('contact/contact.html.twig', array(
+        'form' => $form->createView()
+    ));
+}
   
     /**
      * @Route("/search", name="search")
@@ -36,7 +69,7 @@ class HomePageController extends Controller
         return $this->render('search/search.html.twig');
     }
  
-    /**
+      /**
      * @Route("/sidebar", name="sidebar")
      */
     public function sidebar() {
